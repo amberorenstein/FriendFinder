f535bc36c1927b226de24a1f0cc36927a42f2020
amberorenstein <amber.orenstein@gmail.com>
2017-08-10 17:34:28 -0600

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,10 @@
+//require data
 var friends = require("../data/friends.js");
-
 module.exports = function(app) {
   app.get("/api/friends", function(req,res) {
     res.json(friends);
   });
-
+//post user input
   app.post("/api/friends", function(req,res){
     var bestMatch = {
       name: "",
